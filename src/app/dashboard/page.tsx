e6d3148f5eaa8ebc8e7398bfe510bb92dc766ee5
3amfuxxk <olezeml-ipt25@lll.kpi.ua>
2023-12-13 03:34:28 +0300

    help react-query

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,8 +10,10 @@ import Legend from "../../modules/Dashboard/LegendForChart/LineChartLegend";
 import BarChart from "../../modules/Dashboard/Graphics/BarChart";
 import RadialChart from "../../modules/Dashboard/Graphics/RadialChart";
 import LongCard from "../../modules/Dashboard/LongCard/LongCard";
-import { KPIs, Data } from "../../types/dashboard.types";
+import { Data } from "../../types/dashboard.types";
 import KPICards from "../../modules/Dashboard/KPICards/KPICards";
+import { useQuery, QueryClient, QueryClientProvider } from "react-query";
+import axios from 'axios';
 
 const Page = styled.div`
     display: flex;
@@ -44,81 +46,57 @@ const Content = styled.div`
     gap: 24px;
     width: 100%;
 `
-
-const data: Data = {
-    kpis: {
-      income: 15000,
-      totalBookings: 45000,
-      canceled: 55,
-    },
-    booking: {
-      income: {
-        value: 95000,
-        breakdown: [40000, 50000, 60000, 100000, 90000, 92000, 95000],
-      },
-    },
-    barchart: [
-        {
-            label: 'Pending',
-            value: 56000,
-            percentage: 20,
-            background: '#FFAB00',
-        },
-        {
-            label: 'Canceled',
-            value: 10000,
-            percentage: 56,
-            background:'rgba(255, 86, 48, 1)',
-        },
-        {
-            label: 'Sold',
-            value: 70,
-            percentage: 86,
-            background:'rgba(34, 197, 94, 1)',
-        },
-    ],
-    linechart: {
-        totalIncomes: 9990,
-        percent: 8.2,
-    },
-    smallRadials: {
-        sold: 9990,
-        pendingPayment: 10989,
-        percentSold: 73.9,
-        percentPending: 45.6,
-    },
-    radial: {
-        soldOut: 120,
-        available: 66,
-    }
-  };
+interface OverviewProps {
+    pageTitle: string;
+}
 
 export default function Main() {
     const pageTitle = "Overview";
+    const queryClient = new QueryClient();
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Overview pageTitle={pageTitle} />
+        </QueryClientProvider>
+    );
+}
+
+function Overview({ pageTitle }: OverviewProps) {
+    const fetchData = async () => {
+        try {
+            const response = await axios.get('/api/dashboard-overview');
+            return response.data;
+        } catch (error) {
+            console.error('Ошибка при запросе данных:', error);
+            throw new Error('Не удалось загрузить данные');
+        }
+    };
+
+    const { data } = useQuery('dashboardData', fetchData);
 
     return (
         <Container>
             <DashboardLayout text={pageTitle}>
                 <Page>
-                    <KPICards kpis={data.kpis} />
+                    <KPICards kpis={data?.kpis} />
                     <Content>
                         <Charts>
                             <BigCard background="rgb(209,241,229)">
-                                <Legend kpis={data.linechart} />
+                                <Legend kpis={data?.linechart} />
                                 <TinyLineGraph />
                             </BigCard>
                             <BigCard>
                                 <ChartLegend>
                                     Booked
                                 </ChartLegend>
-                                <BarChart kpis={data.barchart} />
+                                <BarChart kpis={data?.barchart} />
                             </BigCard>
-                            <LongCard kpis={data.smallRadials} />
+                            <LongCard kpis={data?.smallRadials} />
                         </Charts>
-                        <RadialChart kpis={data.radial} />
+                        <RadialChart kpis={data?.radial} />
                     </Content>
                 </Page>
             </DashboardLayout>
         </Container>
-    )
+    );
 }
